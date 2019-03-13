0a61120b851fdaff11b0400c12b6c52ddf0c952b
DjDf Crash <djdf.crash@gmail.com>
2019-03-14 00:59:34 +0200

    Initial commit

---
+++ src/main/java/ua/in/ukravto/phones/views/MainViews.java
@@ -0,0 +1,8 @@
+package ua.in.ukravto.phones.views;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainViews extends VerticalLayout {
+}
